16490aed9cea2ae9d36396253275619174d57fde
JustinBarsalou <justin.barsalou@g.austincc.edu>
2020-01-30 19:19:41 -0600

    got login button to work, basic layout for login page and dashboard are set

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
+// import React from 'react';
 import './App.css';
+import Button from '@material-ui/core/Button'
+import React, { Component } from 'react'
+import ButtonAppBar  from './components/navBar.js'
+import UsernameTextFields from './components/usernameField'
+import PasswordTextFields from './components/passwordField'
+import LoginButton from './components/loginButton'
+import CardOne from './components/cardOne'
+import CardTwo from './components/cardTwo'
+import CardThree from './components/cardThree';
+import './componentStyles/cards.css'
+// import * as navigationBar from './components/navBar.js'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class Music extends Component {
+  
+  constructor(props) {
+    super(props)
+    this.state={
+    loggedIn: false,
+      }
+    // this.logIn = this.logIn.bind(this)
+    
+  }
+
+logIn = (props) => {
+  this.setState({
+    loggedIn: true
+  });
+  console.log(this.loggedIn)
+} 
+  
+  render() {
+
+      if (this.state.loggedIn) {
+        return (
+
+          
+          <div>
+            <div>
+              <ButtonAppBar></ButtonAppBar>
+            </div>
+
+            <div className='cards'>
+              <CardOne></CardOne>
+              <CardTwo></CardTwo>
+              <CardThree></CardThree>
+            </div>
+
+          </div>
+        )
+      }
+      else {
+        return (
+          <div>
+            <div>
+            <ButtonAppBar></ButtonAppBar>
+            </div>
+
+            <div className='center'>
+              <div>
+              <UsernameTextFields></UsernameTextFields>
+              </div>
+
+              <div>
+                <PasswordTextFields></PasswordTextFields>
+              </div>
+
+              <button onClick={this.logIn}>
+              <LoginButton></LoginButton>
+              </button>
+
+            </div>
+
+          </div>
+
+
+        )
+      }
+  }
+
 }
-
-export default App;
